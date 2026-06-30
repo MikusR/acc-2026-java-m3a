e8daaefe99e1424ee53ff4459ecb0123f3095f57
Mikus Rakis <mikus.rakis@gmail.com>
2026-06-30 11:49:18 +0300

    show menu and handle invalid input

--- src/main/java/org/example/menu/ConsoleMenu.java
+++ src/main/java/org/example/menu/ConsoleMenu.java
@@ -7,19 +7,37 @@ import java.util.Scanner;
 
 public class ConsoleMenu {
     private final Shelter<Animal> shelter;
-    private final Scanner scanner =  new Scanner(System.in);
+    private final Scanner scanner = new Scanner(System.in);
+
     public ConsoleMenu(Shelter<Animal> shelter) {
         this.shelter = shelter;
     }
 
-    public void start(){
+    public void start() {
         // TODO:
         // Show menu in a loop
         // Read user input
         // Call correct 'Shelter' methods based on selected option
+
+        do {
+            System.out.println("+".repeat(30));
+            printMenu();
+            if (!scanner.hasNextInt()) {
+                // scanner.next() needs to be here, as the hasNextInt() keeps the wrong input
+                scanner.next();
+                continue;
+            }
+            ;
+            int choice = scanner.nextInt();
+            if (choice == 0) break;
+            System.out.println(choice);
+        } while (true);
+
+        scanner.close();
+
     }
 
-    private void printMenu(){
+    private void printMenu() {
         System.out.println("""
                 1. Add animal
                 2. List all animals
