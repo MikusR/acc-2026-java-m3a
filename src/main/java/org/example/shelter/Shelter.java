84fedb86e4b470ec8137d7bff6949cd325b75606
Mikus Rakis <mikus.rakis@gmail.com>
2026-06-30 12:44:56 +0300

    implemented getAllAnimals, findBySpecies, findAvailableAnimals,

--- src/main/java/org/example/shelter/Shelter.java
+++ src/main/java/org/example/shelter/Shelter.java
@@ -1,33 +1,41 @@
 package org.example.shelter;
 
+import org.example.model.AdoptionStatus;
 import org.example.model.Animal;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Shelter <T extends Animal>{
+public class Shelter<T extends Animal> {
     private final List<T> animals = new ArrayList<>();
 
-    public void addAnimal(T animal){
+    public void addAnimal(T animal) {
         animals.add(animal);
     }
 
-    public List<T> getAllAnimals(){
-        // TODO
-        return null;
+    public List<T> getAllAnimals() {
+
+        return animals;
     }
 
-    public List<T> findBySpecies(String species){
-        // TODO
-        return null;
+    public List<T> findBySpecies(String species) {
+        List<T> animalsBySpecies = new ArrayList<>();
+        for (T animal : animals) {
+            if (animal.getSpecies().equals(species)) animalsBySpecies.add(animal);
+        }
+
+        return animalsBySpecies;
     }
 
-    public List<T> findAvailableAnimals(){
-        // TODO
-        return null;
+    public List<T> findAvailableAnimals() {
+        List<T> availableAnimals = new ArrayList<>();
+        for (T animal : animals) {
+            if (animal.getAdoptionStatus().equals(AdoptionStatus.AVAILABLE)) availableAnimals.add(animal);
+        }
+        return availableAnimals;
     }
 
-    public void markAsAdopted(String id){
+    public void markAsAdopted(String id) {
         // TODO
     }
 }
